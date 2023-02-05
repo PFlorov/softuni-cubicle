2c8b44477cd5a38ddd3e0688cc5c1d1fffcdae92
PFlorov <105749236+PFlorov@users.noreply.github.com>
2023-02-05 17:33:01 +0200

    register user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,18 +1,31 @@
 const router = require("express").Router();
 
-router.get("/login", function (req, res) {
+const authService = require("../services/authService");
+
+router.get("/login", (req, res) => {
   res.render("auth/login");
 });
 
-router.get("/register", function (req, res) {
+router.get("/register", (req, res) => {
   res.render("auth/register");
 });
 
-router.post("/register", function (req, res) {
+router.post("/register", async (req, res) => {
   const { username, password, repeatPassword } = req.body;
+
   if (password !== repeatPassword) {
+    return res.redirect("/404");
+  }
+
+  const existingUser = await authService.getUserByUsername(username);
+
+  if (existingUser) {
     return res.status(404).end();
   }
+
+  const user = await authService.register(username, password);
+
+  res.redirect("/login");
 });
 
 module.exports = router;
