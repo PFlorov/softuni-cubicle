44aabec15134e86418f40399d5540aa05386dc76
PFlorov <105749236+PFlorov@users.noreply.github.com>
2023-02-06 12:23:47 +0200

    generate token on login

--- src/config/index.js
+++ src/config/index.js
@@ -2,10 +2,12 @@ const config = {
   production: {
     PORT: 1234,
     DB_URI: "mongodb://127.0.0.1:27017/cubicle",
+    SECRET: "SOMEPRODSECRET",
   },
   development: {
     PORT: 5001,
     DB_URI: "mongodb://127.0.0.1:27017/cubicle",
+    SECRET: "SOMEDEVSECRET",
   },
 };
 module.exports = config[process.env.node_env || "development"];
